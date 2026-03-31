ee8de07035626d41c5c259bf069c8412ee017be0
Alisson7-dev <alissinlima7713@gmail.com>
2026-03-31 20:01:36 -0300

    Corrige rota alunos e ajusta logout

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'src', 'public')));
 
-app.get("/", (req,res) => {
+app.get("/views/dashboard.html", (req,res) => {
   res.sendFile(path.join(__dirname, "src", "views", "login.html"))
 })
 app.get('/views/dashboard.html', (req, res) => {
