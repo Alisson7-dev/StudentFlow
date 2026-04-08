e36ebdfc7b524b267efdea17396dfb600c49ab55
Alisson <alissinlima7713@gmail.com>
2026-04-08 10:27:01 -0300

    MVP

--- server.js
+++ server.js
@@ -24,3 +24,4 @@ app.use("/dashboard",dashboardRoutes)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>console.log( `Server running on port ${PORT}` ))
+console.log("AUTH ROUTES LOADED")
