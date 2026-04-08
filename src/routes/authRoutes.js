9aa6bf557160e7273c9be0cdee767427b8a0bfe0
Alisson <alissinlima7713@gmail.com>
2026-04-08 10:32:50 -0300

    MVP

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,5 +5,5 @@ const authcontroller = require ("../controllers/authcontroller")
 
 // rota de login 
 router.post("/login", authcontroller.login)
-
+console.log("Auth routes loaded")
 module.exports = router
