351166b67242b66d0339045654f5d782bd8403f2
Alisson <alissinlima7713@gmail.com>
2026-04-08 10:14:22 -0300

    MVP

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("/", authRoutes)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "public", "login.html"));
+});
 app.use(express.static(path.join(__dirname, 'src', 'public')));
 
 
